b6bebb8512a23d89199f283835f7ba220d28df73
Eirik0 <eowigley@gmail.com>
2017-11-29 11:58:59 -0700

    Change minimax to negamax

--- BoardGameEngine/src/analysis/strategy/MinimaxStrategy.java
+++ BoardGameEngine/src/analysis/strategy/MinimaxStrategy.java
@@ -16,10 +16,10 @@ public class MinimaxStrategy<M, P extends IPosition<M, P>> extends AbstractDepth
 
 	@Override
 	public double evaluate(P position, int player, int plies) {
-		return minimax(position, player, 0, plies);
+		return negamax(position, player, 0, plies, player == position.getCurrentPlayer());
 	}
 
-	private double minimax(P position, int player, int ply, int maxPly) {
+	private double negamax(P position, int player, int ply, int maxPly, boolean max) {
 		if (searchCanceled) {
 			return 0;
 		}
@@ -32,40 +32,24 @@ public class MinimaxStrategy<M, P extends IPosition<M, P>> extends AbstractDepth
 			return positionEvaluator.evaluate(position, possibleMoves, player);
 		}
 
+		double bestScore = Double.NEGATIVE_INFINITY;
 		M move;
-		double bestScore;
 		int i = 0;
-		if (player == position.getCurrentPlayer()) { // Max
-			bestScore = Double.NEGATIVE_INFINITY;
-			do {
-				move = possibleMoves.get(i);
-				position.makeMove(move);
-				double score = minimax(position, player, ply + 1, maxPly);
-				position.unmakeMove(move);
+		do {
+			move = possibleMoves.get(i);
+			position.makeMove(move);
+			double negamax = negamax(position, player, ply + 1, maxPly, player == position.getCurrentPlayer());
+			double score = max ? negamax : -negamax;
+			position.unmakeMove(move);
 
-				if (score > bestScore || (AnalysisResult.isDraw(score) && bestScore < 0) || (AnalysisResult.isDraw(bestScore) && score >= 0)) {
-					bestScore = score;
-				}
+			if (score > bestScore || (AnalysisResult.isDraw(score) && bestScore < 0) || (AnalysisResult.isDraw(bestScore) && score >= 0)) {
+				bestScore = score;
+			}
 
-				++i;
-			} while (i < numMoves);
-		} else { // Min
-			bestScore = Double.POSITIVE_INFINITY;
-			do {
-				move = possibleMoves.get(i);
-				position.makeMove(move);
-				double score = minimax(position, player, ply + 1, maxPly);
-				position.unmakeMove(move);
+			++i;
+		} while (i < numMoves);
 
-				if (score < bestScore || (AnalysisResult.isDraw(score) && bestScore > 0) || (AnalysisResult.isDraw(bestScore) && score <= 0)) {
-					bestScore = score;
-				}
-
-				++i;
-			} while (i < numMoves);
-		}
-
-		return bestScore;
+		return max ? bestScore : -bestScore;
 	}
 
 	@Override
