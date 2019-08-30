311638c65c99a2c5ff06e5ddf3b0af1177f09558
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2019-08-29 22:20:04 -0600

    Minimal logic in Photosynthesis position evaluator

--- src/bge/game/photosynthesis/PhotosynthesisPositionEvaluator.java
+++ src/bge/game/photosynthesis/PhotosynthesisPositionEvaluator.java
@@ -1,12 +1,31 @@
 package bge.game.photosynthesis;
 
+import bge.analysis.AnalysisResult;
 import bge.analysis.IPositionEvaluator;
 import bge.igame.MoveList;
 
 public class PhotosynthesisPositionEvaluator implements IPositionEvaluator<IPhotosynthesisMove, PhotosynthesisPosition> {
     @Override
     public double evaluate(PhotosynthesisPosition position, MoveList<IPhotosynthesisMove> possibleMoves) {
-        // TODO Auto-generated method stub
+        if (possibleMoves.size() == 0) {
+            int[] result = position.getResult();
+            int currentPlayer = position.getCurrentPlayer();
+            int currentPlayerScore = result[currentPlayer];
+            boolean draw = true;
+            for (int i = 0; i < result.length; ++i) {
+                if (i != currentPlayer) {
+                    int otherPlayerScore = result[i];
+                    draw = draw && otherPlayerScore == currentPlayerScore;
+                    if (otherPlayerScore > currentPlayer) {
+                        return AnalysisResult.LOSS;
+                    }
+                }
+            }
+            if (draw) {
+                return AnalysisResult.DRAW;
+            }
+            return AnalysisResult.WIN;
+        }
         return 0;
     }
 }
